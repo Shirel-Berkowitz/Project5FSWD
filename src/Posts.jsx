d0c073c1bd70a51690dec4079d11e932aceb5b38
shiffy01 <sscheine@g.jct.ac.il>
2023-05-10 21:53:44 +0300

    todos posts

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,10 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function Posts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem("ourUser"));
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => {const userPosts = json.filter((post) => post.userId === user.id);
+        setPosts(userPosts);})
+  });
+
+  function postPressed(pst){
+    //choose post and make it bold
+  }
+  
+  
   return (
     <>
-      <h1>these are posts</h1>
+    <h2>your posts:</h2>
+    <ul>
+    {posts.map(pst=>(
+      <li>
+        <button onclick={postPressed(pst)}>
+        <h3>{pst.title}</h3>
+        <p>{pst.body}</p>
+        </button>  
+      </li>
+    )
+      
+    )}
+    </ul>
+  
+
     </>
   );
 }
 
+/**
+ left to do:
+ 1.write the function for when the post is clicked
+ 2.have an option to view comments
+ */
 export default Posts;
--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,18 +1,16 @@
-import React, { useState } from "react";
+import React, { useState,  useEffect  } from "react";
 
 function Todos() {
-  const [todos, setTodos] = useState("");
-
-  const handleLogin = async () => {
-    console.log("ok ok ok")
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
     let user = JSON.parse(localStorage.getItem("ourUser"));
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const tt = await response.json();
-    const userTodos = tt.filter((todo) => todo.userId === user.id);
-    setTodos(userTodos);
-    console.log(todos[0].title)
-  }
-  handleLogin()
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => {const userTodos = json.filter((todo) => todo.userId === user.id);
+        setTodos(userTodos);})
+  });
+
+ 
   function setCheck(td){
     if(td.completed){
       return <><input type="checkbox" name={td.id} checked></input>
@@ -24,13 +22,13 @@ function Todos() {
     }
   }
   
-   
-    //const userTodos = tt.filter((todo) => todo.userId === user.id);
-  function func(){
-    setTimeout(() => {
-      
-    }, 3000);
-    return <ul>
+  
+  
+  
+  return (
+    <>
+    <h2>your todos:</h2>
+    <ul>
     {todos.map(td=>(
       <li>
         {setCheck(td)}
@@ -39,19 +37,14 @@ function Todos() {
       
     )}
     </ul>
-  }
-    
-    
   
-  return (
-    <>
-    <h2>your todos:</h2>
-    
-  {func()}
-   
+
     </>
   );
 }
-//NOT FINISHED THIS PAGE
-//DOESNT WORK YET
+/**
+ left to do:
+ 1.fix checkbox so it is possible to uncheck it
+ 2.have an option to sort the todos in different ways
+ */
 export default Todos;
