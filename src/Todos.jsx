7cb4aeaa3565b2632d73ca40c8d07308cb140488
Shirel <shkadosh@g.jct.ac.il>
2023-05-11 15:12:29 +0300

    fix checkbox so it is possible to uncheck it

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,50 +1,57 @@
-import React, { useState,  useEffect  } from "react";
+import React, { useState, useEffect } from "react";
 
 function Todos() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem("ourUser"));
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => {const userTodos = json.filter((todo) => todo.userId === user.id);
-        setTodos(userTodos);})
-  });
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        const userTodos = json.filter((todo) => todo.userId === user.id);
+        setTodos(userTodos);
+      });
+  }, []); // Add an empty dependency array to execute the effect only once
 
- 
-  function setCheck(td){
-    if(td.completed){
-      return <><input type="checkbox" name={td.id} checked></input>
-                <label for={td.id}>{td.title}</label></>
-    }
-    else{
-      return <><input type="checkbox" name={td.id} ></input>
-                <label for={td.id}>{td.title}</label></>
-    }
+  function handleCheckChange(event, td) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === td.id) {
+        return {
+          ...todo,
+          completed: event.target.checked,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
   }
-  
-  
-  
-  
+
+  function setCheck(td) {
+    return (
+      <>
+        <input
+          type="checkbox"
+          name={td.id}
+          checked={td.completed}
+          onChange={(event) => handleCheckChange(event, td)}
+        ></input>
+        <label htmlFor={td.id}>{td.title}</label>
+      </>
+    );
+  }
+
   return (
     <>
-    <h2>your todos:</h2>
-    <ul>
-    {todos.map(td=>(
-      <li>
-        {setCheck(td)}
-      </li>
-    )
-      
-    )}
-    </ul>
-  
-
+      <h2>your todos:</h2>
+      <ul>
+        {todos.map((td) => (
+          <li>{setCheck(td)}</li>
+        ))}
+      </ul>
     </>
   );
 }
 /**
  left to do:
- 1.fix checkbox so it is possible to uncheck it
- 2.have an option to sort the todos in different ways
+ have an option to sort the todos in different ways
  */
 export default Todos;
