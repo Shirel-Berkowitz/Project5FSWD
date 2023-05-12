268f31dc568ed8f8903d60897311c2225416b48b
Shirel <shkadosh@g.jct.ac.il>
2023-05-12 15:34:35 +0300

    bold clicked post

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,43 +1,41 @@
 import React, { useState, useEffect } from "react";
+
 function Posts() {
   const [posts, setPosts] = useState([]);
+  const [selectedPost, setSelectedPost] = useState(null); // New state variable
+
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem("ourUser"));
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => {const userPosts = json.filter((post) => post.userId === user.id);
-        setPosts(userPosts);})
-  });
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        const userPosts = json.filter((post) => post.userId === user.id);
+        setPosts(userPosts);
+      });
+  }, []);
 
-  function postPressed(pst){
-    //choose post and make it bold
+  function postPressed(pst) {
+    setSelectedPost(pst); // Update selected post
   }
-  
-  
+
   return (
     <>
-    <h2>your posts:</h2>
-    <ul>
-    {posts.map(pst=>(
-      <li>
-        <button onclick={postPressed(pst)}>
-        <h3>{pst.title}</h3>
-        <p>{pst.body}</p>
-        </button>  
-      </li>
-    )
-      
-    )}
-    </ul>
-  
-
+      <h2>your posts:</h2>
+      <ul>
+        {posts.map((pst) => (
+          <li key={pst.id}>
+            <button
+              onClick={() => postPressed(pst)}
+              className={selectedPost === pst ? "selectedPost" : ""}
+            >
+              <h3>{pst.title}</h3>
+              <p>{pst.body}</p>
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
 
-/**
- left to do:
- 1.write the function for when the post is clicked
- 2.have an option to view comments
- */
 export default Posts;
